913070a6b1496760bb7bfb16358e210c461302bd
Mino <mino.rakoto94@gmail.com>
2018-06-14 08:35:44 +0300

    master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-	attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
 	before_action :configure_params_permit, if: :devise_controller?
-	after_action :crop_image,only: [:update], if: :devise_controller?
 	protected
 	def configure_params_permit
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:nom, :prenom, :sexe, :date_de_naissance, :adresse, :telephone, :image])
@@ -9,7 +7,4 @@ class ApplicationController < ActionController::Base
 
 	end
 
-	def crop_image
-		image.recreate_versions! if crop_x.present?
-	end
 end
